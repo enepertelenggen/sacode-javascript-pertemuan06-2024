74946862899a32296014efe9804394ca1b297676
Eneper <enepertelenggen@gmail.com>
2024-06-07 19:56:45 +0900

    file index dan main

---
+++ main.js
@@ -0,0 +1,14 @@
+
+const str1 = "eneper telenggenn";
+
+const str2 = "eneper telenggen";
+
+console.log(str1);
+console.log(str2);
+
+const str3 =`${str2}`;
+
+console.log(str1.toLowerCase());
+console.log(str1);
+console.log(str2);
+console.log(str3);
